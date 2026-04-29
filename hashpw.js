087e36b1656f6f5efcfec5bb2bf43957ee7279ec
jessicuh12 <gabriellaprdd@gmail.com>
2026-04-30 02:56:49 +0800

    fixed package lock.json

--- hashpw.js
+++ hashpw.js
@@ -9,7 +9,7 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT) || 5432,
+  port: Number(process.env.DB_PORT) || 5433,
 });
 
 async function hashPasswords() {
